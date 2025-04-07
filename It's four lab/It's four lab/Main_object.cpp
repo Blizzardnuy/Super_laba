e717a315d8d9699dae06e51b9763795c6ea4d569
Blizzardnuy <dansizov5@gmail.com>
2025-04-07 16:12:42 +0300

    init: add lab foulder

---
+++ It's four lab/It's four lab/Main_object.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	double a, b, c;
+	cout << "Enter values for variables A, B and C: ";
+	cin >> a >> b >> c;
+	if (a > b && b > c || a < b && b < c) {
+		a *= 2;
+		b *= 2;
+		c *= 2;
+		cout << "Meaning of A: " << a << endl << "Meaning of B: " << b << endl << "Meaning of C: " << c << endl;
+	}
+	else {
+		a *= -1;
+		b *= -1;
+		c *= -1;
+		cout << "Meaning of A: " << a << endl << "Meaning of B: " << b << endl << "Meaning of C: " << c << endl;
+	}
+}
